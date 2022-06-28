20448b86b178d52d69adb0f847d0686165839e2c
JPDevM <devecchi@exxo.es>
2022-06-28 13:30:56 +0200

    add project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,80 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import poweredBy from "./powered-by-vitawind-dark.png";
+// Dependencies
+import React, { Fragment, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+// import 'dotenv/config';
+import ReactGA from 'react-ga';
+const TRACKING_ID = 'UA-225380906-1'; // Google Analytics Tracking ID
+ReactGA.initialize(TRACKING_ID);
+import TagManager from 'react-gtm-module';
+const tagManagerArgs = { gtmId: 'GTM-P6GNNFM' }; // Google Tag Manager Tracking ID
+TagManager.initialize(tagManagerArgs);
+// Middlewares
+
+// Components
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Trial from './pages/Trial';
+import Aplications from './pages/Aplications';
+import Tools from './pages/Tools';
+import BackOffice from './pages/BackOffice';
+import Survey from './pages/Survey';
+import ResetPassword from './pages/ResetPassword';
+import TermsAndCond from './pages/termsAndCond';
+import Privacy from './pages/Privacy';
+import Forms from './pages/Forms';
+import NotFoundPage from './pages/NotFoundPage';
+
+
+// Styles
+import './css/style.scss';
+import AOS from 'aos';
+
+// Media
 
 function App() {
-  const [count, setCount] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
+  }, [location.pathname]); // triggered on route change
 
   return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <img
-          src={logo}
-          className="animate-speed h-60 motion-safe:animate-spin"
-          alt="logo"
-        />
-        <style>
-          {
-            "\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          "
-          }
-        </style>
-        <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
-          Vite + React + Tailwindcss v3
-        </p>
-        <p className="mt-3">
-          <button
-            type="button"
-            className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code className="text-[#8d96a7]">App.jsx</code> and save to test
-          HMR updates.
-        </p>
-        <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-        <img src={poweredBy} className="mx-auto my-8" alt="powered-by" />
-      </header>
-    </div>
+    <Fragment>
+      <Routes>
+        {/* Landing */}
+        <Route exact path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route exact path="signin" element={<SignIn />} />
+          <Route exact path="signup" element={<SignUp />} />
+          <Route exact path="trial" element={<Trial />} />
+          <Route exact path="aplications" element={<Aplications />} />
+          <Route exact path="tools" element={<Tools />} />
+          <Route exact path="backoffice" element={<BackOffice />} />
+          <Route exact path="survey" element={<Survey />} />
+          <Route exact path="reset-password" element={<ResetPassword />} />
+          <Route exact path="term-and-cond" element={<TermsAndCond />} />
+          <Route exact path="privacy" element={<Privacy />} />
+          {/* Main */}
+          <Route exact path="forms" element={<Forms />} />
+          {/* 404 Last route */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+        {/* 404 rounte */}
+      </Routes>
+    </Fragment>
   );
 }
 
